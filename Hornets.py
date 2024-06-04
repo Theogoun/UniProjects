8fbf131acad573f7d9ce72832ae9618d58e1fcfe
Theogoun <gountheo03@gmail.com>
2024-06-04 05:43:32 +0300

    fixed distance and made kills better

--- Hornets.py
+++ Hornets.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random as rn
 
-#We make the hornet nests, first element x, second y, third the number of hornets
+#We make the hornet nests [x, y, n]
 A = []
 A.append([25, 65, 100])
 A.append([23, 8, 200])
@@ -19,13 +19,20 @@ A.append([34, 23, 967])
 dmax = 106.61
 
 def Kills(B):
-    kills = 0
+    total_kills = 0
+    remaining_nests = [nest[:] for nest in A]
+
     for i in range(12):
         n = A[i][2]
-        d = np.sqrt(np.power(B[0] + A[i][0],2) + np.power(B[1] + A[i][1],2))
-        kills += n * dmax / (20 * d + 0.00001)
-    return np.round(kills)
+        if n >0:
+            d = np.sqrt((B[0] - remaining_nests[i][0])**2 + (B[1] - remaining_nests[i][1])**2)
+            kills = n * dmax / (20 * d + 0.00001)
+            actual_kills = min(kills, remaining_nests[i][2])
+            total_kills += actual_kills
+            remaining_nests[i][2] -= np.round(actual_kills)
+    return np.round(total_kills)
 
+#30,20  70,80  80,0
 B1 = [rn.randint(0,101),rn.randint(0,101)]
 B2 = [rn.randint(0,101),rn.randint(0,101)]
 B3 = [rn.randint(0,101),rn.randint(0,101)]
